a2279940969d8abdcc005dd8bd36fc3a8eeae521
DheerajGaur.Official <dheeraj.ajaygaur05@gmail.com>
2025-08-04 14:09:41 +0530

    server changes

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 7000;
 
 // Middleware
 app.use(cors());
